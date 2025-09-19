47d058fb41c9628b7eab91317b5087b8abcf99a2
DuyKhanh <duykhanh18102004@gmail.com>
2025-09-19 21:53:08 +0700

    new

--- src/Client/Components/HeaderClient/HeaderClient.tsx
+++ src/Client/Components/HeaderClient/HeaderClient.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";  // Import NavLink thay vì Link
+import { NavLink } from "react-router-dom"; 
 import { assets } from "src/Assets/assets";
 
 const Header = () => (
